08c9d74abf80906c8edcfe5605d33cdfedd0171f
Pedro Lima <pedro.oficial1969@gmail.com>
2023-12-20 18:55:31 -0300

    criacao das DTOs e da entity do entidade products.

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,12 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+  name: string;
+  brand: string;
+  sku: string;
+  amount: string;
+  measurement_unit: string;
+  expired_date: Date;
+  last_purchase_date: Date;
+  price: number;
+  has_stock: boolean;
+  ps: string;
+}
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,13 @@
-export class Product {}
+export class Product {
+  id: number;
+  name: string;
+  brand: string;
+  sku: string;
+  amount: string;
+  measurement_unit: string;
+  expired_date: Date;
+  last_purchase_date: Date;
+  price: number;
+  has_stock: boolean;
+  ps: string;
+}
