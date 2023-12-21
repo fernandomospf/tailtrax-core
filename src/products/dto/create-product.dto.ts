cf43691121c89e85dcef15b667147f41af838169
Pedro Lima <pedro.oficial1969@gmail.com>
2023-12-20 22:11:50 -0300

    validaçoes

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,12 +1,45 @@
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   name: string;
+
+  @IsString()
   brand: string;
+
+  @IsString()
   sku: string;
+
+  @IsString()
+  @IsPositive()
   amount: string;
+
+  @IsString()
+  @IsNotEmpty()
   measurement_unit: string;
+
+  @IsDate()
   expired_date: Date;
+
+  @IsDate()
   last_purchase_date: Date;
+
+  @IsNumber()
   price: number;
+
+  @IsBoolean()
   has_stock: boolean;
+
+  @IsString()
   ps: string;
 }
